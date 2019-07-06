c6a6a0ad0a7feffca85e09c4333b895761ac15a4
Anderson Vargas <anvargasa@unal.edu.co>
2019-07-06 13:39:15 -0500

    Prueba persona.java

--- src/datos/Persona.java
+++ src/datos/Persona.java
@@ -3,10 +3,15 @@ public abstract class Persona {
     private String nombre;
     private String usuario;
     private String contraseña;
+    private boolean activo;
     
    public Persona(String nombre, String usuario, String contraseña){
        this.nombre = nombre;
        this.usuario = usuario;
        this.contraseña = contraseña;
-   }
+   }  
+    public void setActivo(boolean activito) {
+        this.activo = activito;
+    }   
+   
 }
