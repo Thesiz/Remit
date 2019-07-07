5769ce5d49fe05fe2a0d80f2527fdbf424c7c60f
Anderson Vargas <anvargasa@unal.edu.co>
2019-07-07 11:49:06 -0500

    -

--- src/logica/Ejecucion.java
+++ src/logica/Ejecucion.java
@@ -8,6 +8,7 @@ public class Ejecucion {
          /*++++++++++INICIO DE SESION Y REGISTRO++++++++++*/
         Controlador.registrar();
         Controlador.iniciarSesion();
+        /*++++++++++++++++++++++++++++++++++++++++++++++++*/
 
         Usuario temp1 = new Usuario("temp1", "temp1", "love01", true);
         Usuario temp2 = new Usuario("temp2", "temp2", "love01", true);
