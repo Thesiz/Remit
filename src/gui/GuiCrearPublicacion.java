e3933accd0d036e1546b663098ccad427fe22cac
Anderson Vargas <anvargasa@unal.edu.co>
2019-07-14 10:14:49 -0500

    Actualizaciones

--- src/gui/GuiCrearPublicacion.java
+++ src/gui/GuiCrearPublicacion.java
@@ -85,6 +85,7 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
     private void initComponents() {
 
         btngBotones = new javax.swing.ButtonGroup();
+        jPanel1 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         lblTituloPanel = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
@@ -99,9 +100,22 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         txtDescripcion = new javax.swing.JTextArea();
         btnAdjuntar = new javax.swing.JButton();
-        btnCancelar1 = new javax.swing.JButton();
-        btnRemit = new javax.swing.JButton();
         lblArchivo = new javax.swing.JLabel();
+        jPanelRemit = new javax.swing.JPanel();
+        btnRemit = new javax.swing.JButton();
+        jPanelCancelar = new javax.swing.JPanel();
+        btnCancelar1 = new javax.swing.JButton();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Remit");
@@ -142,6 +156,8 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
         btngBotones.add(rbtTutoria);
         rbtTutoria.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         rbtTutoria.setText("Tutoría");
+        rbtTutoria.setContentAreaFilled(false);
+        rbtTutoria.setFocusPainted(false);
         rbtTutoria.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 rbtTutoriaActionPerformed(evt);
@@ -151,6 +167,8 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
         btngBotones.add(rbtGrupoEstudio);
         rbtGrupoEstudio.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         rbtGrupoEstudio.setText("Grupo de estudio autonomo");
+        rbtGrupoEstudio.setContentAreaFilled(false);
+        rbtGrupoEstudio.setFocusPainted(false);
         rbtGrupoEstudio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 rbtGrupoEstudioActionPerformed(evt);
@@ -160,6 +178,7 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
         btngBotones.add(rbtEventoE);
         rbtEventoE.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         rbtEventoE.setText("Evento Estudiantil");
+        rbtEventoE.setFocusPainted(false);
         rbtEventoE.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 rbtEventoEActionPerformed(evt);
@@ -169,6 +188,8 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
         btngBotones.add(rbtVentaServicio);
         rbtVentaServicio.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         rbtVentaServicio.setText("Venta y/o Servicio");
+        rbtVentaServicio.setContentAreaFilled(false);
+        rbtVentaServicio.setFocusPainted(false);
         rbtVentaServicio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 rbtVentaServicioActionPerformed(evt);
@@ -181,29 +202,45 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
 
         btnAdjuntar.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         btnAdjuntar.setText("   Adjuntar imagen");
+        btnAdjuntar.setContentAreaFilled(false);
+        btnAdjuntar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnAdjuntar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnAdjuntarActionPerformed(evt);
             }
         });
 
-        btnCancelar1.setBackground(new java.awt.Color(51, 153, 255));
+        jPanelRemit.setBackground(new java.awt.Color(237, 191, 23));
+        jPanelRemit.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanelRemit.setPreferredSize(new java.awt.Dimension(63, 25));
+        jPanelRemit.setLayout(null);
+
+        btnRemit.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        btnRemit.setText("Remit");
+        btnRemit.setBorderPainted(false);
+        btnRemit.setContentAreaFilled(false);
+        jPanelRemit.add(btnRemit);
+        btnRemit.setBounds(0, 0, 63, 25);
+
+        jPanelCancelar.setBackground(new java.awt.Color(86, 180, 215));
+        jPanelCancelar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanelCancelar.setPreferredSize(new java.awt.Dimension(79, 25));
+        jPanelCancelar.setLayout(null);
+
+        btnCancelar1.setBackground(new java.awt.Color(109, 207, 252));
         btnCancelar1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         btnCancelar1.setText("Cancelar");
+        btnCancelar1.setBorderPainted(false);
+        btnCancelar1.setContentAreaFilled(false);
+        btnCancelar1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCancelar1.setFocusPainted(false);
         btnCancelar1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnCancelar1ActionPerformed(evt);
             }
         });
-
-        btnRemit.setBackground(new java.awt.Color(255, 204, 0));
-        btnRemit.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        btnRemit.setText("Remit");
-        btnRemit.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRemitActionPerformed(evt);
-            }
-        });
+        jPanelCancelar.add(btnCancelar1);
+        btnCancelar1.setBounds(0, 0, 78, 24);
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
@@ -221,9 +258,9 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lblArchivo, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnCancelar1)
+                        .addComponent(jPanelCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(btnRemit)
+                        .addComponent(jPanelRemit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(19, 19, 19))
                     .addGroup(jPanel4Layout.createSequentialGroup()
                         .addComponent(jLabel4)
@@ -232,7 +269,7 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
                         .addComponent(rbtTutoria)
                         .addGap(74, 74, 74)
                         .addComponent(rbtGrupoEstudio)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
                         .addComponent(rbtEventoE)
                         .addGap(81, 81, 81)
                         .addComponent(rbtVentaServicio)))
@@ -259,14 +296,19 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblArchivo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addComponent(btnAdjuntar)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addComponent(jPanelRemit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel4Layout.createSequentialGroup()
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnAdjuntar)
-                            .addComponent(btnRemit)
-                            .addComponent(btnCancelar1))
-                        .addGap(0, 8, Short.MAX_VALUE)))
-                .addContainerGap())
+                            .addComponent(lblArchivo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(jPanel4Layout.createSequentialGroup()
+                                .addComponent(jPanelCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 11, Short.MAX_VALUE)))
+                        .addContainerGap())))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -286,25 +328,6 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnRemitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemitActionPerformed
-        if ((txtTitulo.getText()).equals("") || (txtDescripcion.getText()).equals("")) {
-            JOptionPane.showMessageDialog(this, "Los campos no pueden estar vacíos", "Error",
-                    JOptionPane.ERROR_MESSAGE, (new ImageIcon(getClass().getResource("/recursos/img/x.png"))));
-        } else {
-            Date fecha = new Date();
-            Usuario usuario = new Usuario("a", "a", "a", "a", true);
-            if (lblArchivo.getText().equals("")) {
-                usuario.agregar(setCategoria(), usuario, fecha, txtTitulo.getText(),
-                        txtDescripcion.getText());
-                this.dispose();
-            } else {
-                usuario.agregar(setCategoria(), usuario, fecha, txtTitulo.getText(),
-                        txtDescripcion.getText(), lblArchivo.getText());
-                this.dispose();
-            }
-        }
-    }//GEN-LAST:event_btnRemitActionPerformed
 
     private void btnCancelar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelar1ActionPerformed
         String[] opciones = {"Si", "No"};
@@ -470,8 +493,11 @@ public class GuiCrearPublicacion extends javax.swing.JFrame {
     private javax.swing.JButton btnRemit;
     private javax.swing.ButtonGroup btngBotones;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanelCancelar;
+    private javax.swing.JPanel jPanelRemit;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator3;
     private javax.swing.JSeparator jSeparator4;
